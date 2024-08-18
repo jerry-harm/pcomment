1b5733f232cd818885b28c4bf6d6264eab28b98d
jerry <jestar@no.reply>
2024-08-18 23:34:18 +0800

    测试成功的页面基本框架渲染

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,render_template
 import sqlite3
 
 app = Flask(__name__)
@@ -10,6 +10,33 @@ def hello_world():
 @app.route('/post/<id>', methods=['POST'])
 def get_post(id):
     return f"{id}"
+
+@app.route('/test')
+def test():
+    data=[
+        {'name':'1',
+        'content':'test1',
+        'time':'1',
+        'replays':[
+            {'name':'2',
+        'content':'test2',
+        'time':'2',
+        'replays':[]
+        }
+        ]},
+        {'name':'3',
+        'content':'test3',
+        'time':'3',
+        'replays':[
+            {'name':'4',
+        'content':'test4',
+        'time':'4',
+        'replays':[]
+        }
+        ]}
+    ]
+    return render_template('base.html',comments=data,post='test')
+
 
 if __name__ == '__main__':
     app.run()
