f2721f8343e666ea20bdf9db3e778c5e2582ddb8
jerry <jestar@no.reply>
2024-08-20 18:52:46 +0800

    完成评论和回复功能

--- server.py
+++ server.py
@@ -1,42 +1,91 @@
-from flask import Flask,render_template
-import sqlite3
+import os
+import sys
+
+from datetime import datetime
+
+from flask import Flask, abort, redirect, request, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import DateTime, ForeignKey, Integer, String, Text
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+
+WIN = sys.platform.startswith('win')
+if WIN:  # 如果是 Windows 系统，使用三个斜线
+    prefix = 'sqlite:///'
+else:  # 否则使用四个斜线
+    prefix = 'sqlite:////'
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
+db = SQLAlchemy(app)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+class Comment(db.Model):  
+    id= mapped_column(Integer,primary_key=True)
+    name = mapped_column(String(60),default='anonymous')
+    content = mapped_column(Text,default='nothing...')
+    date = mapped_column(DateTime,default=datetime.now,onupdate=datetime.now)
+    replay_id = mapped_column(Integer,ForeignKey("comment.id"),nullable=True) # 空白时作为根comment
 
-@app.route('/post/<id>', methods=['POST'])
-def get_post(id):
-    return f"{id}"
+    def get_replays(self):
+        # 查询并返回
+        data=[]
+        replays =  db.session.execute(db.select(Comment).filter_by(replay_id=self.id)).scalars()
+        for replay in replays:
+            print(replay.id)
+            data.append(replay.to_dict())
+        return data
+        
+    def to_dict(self):
+        return {
+                'id':self.id,
+                'name':self.name,
+                'content':self.content,
+                'time':self.date,
+                'replay':self.get_replays()
+                }
 
-@app.route('/test')
+
+@app.post("/post")
+def post():
+    comment=Comment(content=request.form.get('content'),name=request.form.get('name'))
+    db.session.add(comment)
+    db.session.commit()
+    # 管理员创建根post
+    abort(200)
+
+
+
+@app.get("/comment/<int:id>")
+def get_comment(id):
+    post = db.get_or_404(Comment,id)
+
+    data=post.get_replays()
+    # 返回某个post的评论
+    return render_template('base.html',comments=data,post=post)
+    
+
+@app.post("/comment/<int:id>")
+def post_comment(id):
+    # 评论给id
+    post = db.get_or_404(Comment,id)
+    print('1')
+    comment = Comment(content=request.form.get('content'),replay_id=post.id,name=request.form.get('name'))
+    db.session.add(comment)
+    db.session.commit()
+    
+    # 返回这个comment所在的post
+    return redirect(url_for('get_comment',id=post.id))
+
+@app.route("/test")
 def test():
-    data=[
-        {'name':'1',
-        'content':'test1',
-        'time':'1',
-        'replays':[
-            {'name':'2',
-        'content':'test2',
-        'time':'2',
-        'replays':[]
-        }
-        ]},
-        {'name':'3',
-        'content':'test3',
-        'time':'3',
-        'replays':[
-            {'name':'4',
-        'content':'test4',
-        'time':'4',
-        'replays':[]
-        }
-        ]}
-    ]
-    return render_template('base.html',comments=data,post='test')
+    comment=Comment(content='content',name='name')
+    db.session.add(comment)
+    db.session.commit()
+    # 管理员创建根post
+    return "created"
 
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run()
