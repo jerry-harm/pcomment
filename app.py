4dac7cac2d2826badf9e3be256aba03a8977770f
jerry <jestar@no.reply>
2024-08-20 21:48:31 +0800

    完成首页功能，拆分html模板

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from email.policy import default
 import os
 import sys
 
@@ -8,6 +9,7 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import DateTime, ForeignKey, Integer, String, Text
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
+import click
 
 WIN = sys.platform.startswith('win')
 if WIN:  # 如果是 Windows 系统，使用三个斜线
@@ -22,6 +24,7 @@ db = SQLAlchemy(app)
 class Comment(db.Model):  
     id= mapped_column(Integer,primary_key=True)
     name = mapped_column(String(60),default='anonymous')
+    title = mapped_column(String(60),nullable=True)
     content = mapped_column(Text,default='nothing...')
     date = mapped_column(DateTime,default=datetime.now,onupdate=datetime.now)
     replay_id = mapped_column(Integer,ForeignKey("comment.id"),nullable=True) # 空白时作为根comment
@@ -38,37 +41,35 @@ class Comment(db.Model):
     def to_dict(self):
         return {
                 'id':self.id,
+                'title':self.title,
                 'name':self.name,
                 'content':self.content,
-                'time':self.date,
+                'date':self.date,
                 'replay':self.get_replays()
                 }
 
 
-@app.post("/post")
-def post():
-    comment=Comment(content=request.form.get('content'),name=request.form.get('name'))
-    db.session.add(comment)
-    db.session.commit()
-    # 管理员创建根post
-    abort(200)
+@app.get("/")
+def index():
+    posts = db.session.execute(db.select(Comment).filter_by(replay_id=None)).scalars()
+
+    return render_template("index.html",comments=posts)
+
 
 
 
 @app.get("/comment/<int:id>")
 def get_comment(id):
     post = db.get_or_404(Comment,id)
-
     data=post.get_replays()
     # 返回某个post的评论
-    return render_template('base.html',comments=data,post=post)
+    return render_template('comment.html',comments=data,post=post)
     
 
 @app.post("/comment/<int:id>")
 def post_comment(id):
     # 评论给id
     post = db.get_or_404(Comment,id)
-    print('1')
     comment = Comment(content=request.form.get('content'),replay_id=post.id,name=request.form.get('name'))
     db.session.add(comment)
     db.session.commit()
@@ -76,14 +77,39 @@ def post_comment(id):
     # 返回这个comment所在的post
     return redirect(url_for('get_comment',id=post.id))
 
-@app.route("/test")
-def test():
-    comment=Comment(content='content',name='name')
-    db.session.add(comment)
-    db.session.commit()
-    # 管理员创建根post
-    return "created"
 
+@app.cli.command('create')
+@click.argument("content")
+@click.option('--title',default=None)
+@click.option('--name',default=None)
+@click.option('--replay_id',default=None)
+def create_post(content,title,name,replay_id):
+    with app.app_context():
+        comment=Comment(content=content,name=name,title=title,replay_id=replay_id)
+        db.session.add(comment)
+        db.session.commit()
+    print('created')
+
+@app.cli.command('change',help='change and show')
+@click.argument("id")
+@click.option('--content',default=None)
+@click.option('--title',default=None)
+@click.option('--name',default=None)
+@click.option('--replay_id',default=None)
+def change_post(id,content,title,name,replay_id):
+    with app.app_context():
+        comment=db.get_or_404(Comment,id)
+        if content:
+            comment.content=content
+        if title:
+            comment.title=title
+        if name:
+            comment.name=name
+        if replay_id:
+            comment.replay_id=replay_id
+        print(comment.__dict__)
+        db.session.commit()
+    print('modified')
 
 if __name__ == '__main__':
     with app.app_context():
